a6de3179a7490c9932d435170b303ee97bd3d079
hungk4 <nguyenmanhhung15022004@gmail.com>
2025-08-27 15:45:46 +0700

    reponsive homepage

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,18 +1,19 @@
 import "./Section.css";
+import { Row, Col } from "antd";
 
 function Section(props) {
   const renderMedia = () => {
-    switch(props.mediaType) {
-      case 'image':
+    switch (props.mediaType) {
+      case "image":
         return <img src={props.media} alt={props.title} />;
-      case 'video':
+      case "video":
         return (
           <video controls>
             <source src={props.media} type="video/mp4" />
             Trình duyệt của bạn không hỗ trợ video.
           </video>
         );
-      case 'iframe':
+      case "iframe":
         return props.media;
       default:
         return null;
@@ -24,14 +25,15 @@ function Section(props) {
         <h3 className="h3">{props.title}</h3>
         <div className="line"></div>
       </div>
-      <div className="section-content">
-        <div className="section-text p1-r">
+
+      <Row gutter={[32, 32]} className="section-content" align="middle">
+        <Col xs={24} md={12} className="section-text p1-r">
           {props.text ? <p>{props.text}</p> : <p>Nội dung đang cập nhật....</p>}
-        </div>
-         <div className="section-media">
+        </Col>
+        <Col xs={24} md={12} className="section-media">
           {props.media && renderMedia()}
-        </div>
-      </div>
+        </Col>
+      </Row>
     </section>
   );
 }
